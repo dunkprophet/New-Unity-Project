2deaff08e29918426d0ad2347a7163be2554b7b8
glitch951 <chris_7722@hotmail.com>
2015-06-06 19:56:31 +0200

    Overworld attempt
    Kind of a failure

---
+++ Assets/Scripts/OverworldManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class OverworldManager : MonoBehaviour {
+	public static OverworldManager instance;
+
+	/*
+	
+	public GameObject TilePrefab;
+	public GameObject UserPlayerPrefab;
+	public GameObject AIPlayerPrefab;
+	
+	
+	public int mapSize = 11;
+	
+	*/
+
+	// Use this for initialization
+	void Start () {	
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+	}
+
+}
---
+++ Assets/Scripts/OverworldPlayer.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OverworldPlayer : MonoBehaviour {
+	
+	//float speed = 3.0f;
+	RaycastHit hit;
+	Vector3 newPos;
+
+	bool canWalk = false;
+	
+	public float speed = 3.0f;
+	public float rotateSpeed = 3.0f;
+
+	// Use this for initialization
+	void Start () {
+		hit = new RaycastHit();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		/*Input.GetButton ("w");
+		var move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis(""), 0);
+		transform.position += move * speed * Time.deltaTime;
+		*/
+		if (canWalk)
+		{
+			makeCharacterWalk();
+		}
+
+		Debug.Log(transform.position);
+
+	}
+	void OnGUI()
+	{
+		Event e = Event.current;
+		
+		if (e.button == 0 && e.isMouse)
+		{
+			Debug.Log("mouse click");
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			if (Physics.Raycast(ray, out hit, 1000.0f))
+			{
+				newPos = new Vector3(hit.point.x, 0.5f, hit.point.z);    
+			}
+			
+			canWalk = true;
+			
+			//point = Input.mousePosition;
+			//Debug.Log("point is " + point);
+		}
+	}
+	void makeCharacterWalk()
+	{
+		CharacterController controller = GetComponent<CharacterController>();
+		
+		Vector3 diff = transform.TransformDirection(newPos - transform.position);
+		controller.SimpleMove(diff * speed);    
+	}
+}
