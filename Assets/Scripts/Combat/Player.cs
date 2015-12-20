81999c279bd1321409600ef93d7df9a0ef9c644e
glitch951 <christofferbodegrd@gmail.com>
2015-12-20 13:53:35 +0100

    Fixed it
    :jp:

--- Assets/Scripts/Combat/Player.cs
+++ Assets/Scripts/Combat/Player.cs
@@ -3,13 +3,34 @@ using System.Collections;
 
 public class Player : MonoBehaviour {
 
+	public float moveSpeed =10.0f;
+
+	public Vector3 moveDestination;
+
+	private int moves;
+	private int movesPerMove;
+
+	void Awake ()
+	{
+		moveDestination = transform.position;
+
+	}
+
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
 	
+	}
+
+	public virtual void TurnUpdate()
+	{
+
+
 	}
 }
